cd0440092a6780498d60b4e1fc335d7623d75052
Ndagi Stanley <ndagis@gmail.com>
2020-06-22 18:47:30 +0300

    Editor - Create_a_Custom_repository

---
+++ src/product/product.repository.ts
@@ -0,0 +1,34 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { Product } from './product.entity';
+import { CreateProductDTO } from './dto/create-product.dto';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+
+    public async createProduct(
+        createProductDto: CreateProductDTO,
+    ): Promise<Product> {
+        const { name, description, price } = createProductDto;
+
+        const product = new Product();
+        product.name = name;
+        product.description = description;
+        product.price = price;
+
+        await product.save();
+        return product;
+    }
+
+    public async editProduct(
+        createProductDto: CreateProductDTO,
+        editedProduct: Product,
+    ): Promise<Product> {
+        const { name, description, price } = createProductDto;
+
+        editedProduct.name = name;
+        editedProduct.description = description;
+        editedProduct.price = price;
+        await editedProduct.save();
+
+        return editedProduct;
+    }
